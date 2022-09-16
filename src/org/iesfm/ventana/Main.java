a4dc649848d200c6c9759d7fd8e2253f43a33a9b
GabrielC <Pukssito@gmail.com>
2022-09-16 10:23:28 +0200

    mi primera ventana

---
+++ src/org/iesfm/ventana/Main.java
@@ -0,0 +1,40 @@
+package org.iesfm.ventana;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*JFrame f = new JFrame(); // creating instance of JFrame
+        f.setTitle("Panel Redondeado");
+        f.setBounds(10,10,400,400); //establece el tamaño de la ventana
+        f.setVisible(true); //hace visible la ventana
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//cierra la ventana
+        //este es el panel que contiene los botones
+        JPanel panel = new JPanel();    //crea un panel
+        //panel.setBackground(Color.RED); //establece el color de fondo del panel
+        JLabel label = new JLabel("Usuario"); // crea un label
+        //label.setForeground(Color.white); //establece el color de las letras
+        panel.add(label); //añade el label al panel
+        f.setContentPane(panel); //añade el panel a la ventana
+
+        JTextField tf = new JTextField(16); //crea un campo de texto
+        JButton b = new JButton("Aceptar"); //crea un boton
+        panel.add(tf); //añade el campo de texto al panel
+        panel.add(b); //añade el boton al panel
+        f.repaint(); //redibuja la ventana
+        f.revalidate(); //revalida la ventana */
+
+
+        JFrame f = new JFrame();
+        f.setTitle("Panel Redondeado");
+        f.setBounds(10,10,400,400);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel = new JPanel();
+        JPanel panel2 = new JPanel();
+
+
+    }
+}
